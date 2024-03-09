30039553316e2aa7900ebd1c2c3b58fc965fe8e6
boq2abt <adil.bouqfoul2@bosch.com>
2024-03-09 15:19:36 +0100

    simple-java-maven-app example from jkenkins doc

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,13 +2,18 @@ package com.mycompany.app;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) throws Exception
-    {
-        Thread.sleep(10);
-        System.out.println( "Hello World!" );
+public class App {
+
+    private static final String MESSAGE = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
     }
 }
